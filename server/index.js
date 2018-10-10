db64ac506c593d678cfe399523f8ba9d30aca271
bear-wolf <bear-wolf@ukr.net>
2018-10-10 11:10:30 +0300

    Server: run

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-require('app');
+/**
+ * Created by andrew on 2/1/17.
+ */
+var app,
+    http = require('http'),
+    express = require('express'),
+    bodyParser = require('body-parser');
+
+app = express();
+app.set('port', 3001);
+
+
+http.createServer(app).listen(app.get('port'), function () {
+    var routes = require('./routes/index').createInstance();
+
+    console.log('Express server listening on port '+ app.get('port'));
+
+    // app.use(bodyParser.urlencoded({
+    //     extended: true
+    // }))
+    // app.use(bodyParser.json()) // parse application/json
+
+    routes.assignRoutes(app);
+})
+process.on('exit', function (code) {
+    console.log('About to exit with code:'+code);
+});
+process.on('finish', function() {
+    console.log('request end');
+});
