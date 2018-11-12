6153e161c10986b8ec49e40de8a89e06aa6de2a4
bear-wolf <bear-wolf@ukr.net>
2018-11-12 16:44:15 +0200

    Profile: add route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -11,6 +11,41 @@ var RouteUser = {
                 .setResponce(res)
                 .actionGet();
         });
+
+        // verify
+        app.get('/room/:id', function (req, res) {
+            profileController
+                .createInstance()
+                .setRequest(req)
+                .setResponce(res)
+                .actionGet();
+        });
+
+        //INSERT // verify
+        //users/123/accounts
+        app.post('/room/', function (req, res) {
+            roomController
+                .createInstance()
+                .setRequest(req)
+                .setResponce(res)
+                .actionSave();
+        });
+        //UPDATE // verify
+        app.post('/room/:id', function (req, res) {
+            roomController
+                .createInstance()
+                .setRequest(req)
+                .setResponce(res)
+                .actionSave();
+        });
+        //DELETE // curl -X "DELETE" http://www.url.com/page // verify
+        app.delete('/room/:id', function (req, res) {
+            roomController
+                .createInstance()
+                .setRequest(req)
+                .setResponce(res)
+                .actionRemove();
+        });
     }
 }
 
