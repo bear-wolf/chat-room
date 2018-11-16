9326b4728c233255f96329649d5937911561d63a
bear-wolf <bear-wolf@ukr.net>
2018-11-16 12:24:21 +0200

    auth service: add

--- front/src/app/modules/shared/services/auth.service.ts
+++ front/src/app/modules/shared/services/auth.service.ts
@@ -1,13 +1,52 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject, Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthModel} from "../models/auth";
+import {StorageService} from "./storage.service";
+import {UserModel} from "../models/user";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
+    headers: HttpHeaders = new HttpHeaders();
 
-    constructor(private httpClient: HttpClient) { }
+    private auth = new BehaviorSubject<AuthModel>(new AuthModel());
+    public dataAuthentication = this.auth.asObservable();
+
+    constructor(
+        private httpClient: HttpClient,
+        private storageService: StorageService) {
+    }
+
+    updateAuthentication(data: AuthModel){
+        return this.auth.next(data);
+    }
+
+    isAuthenticate(): Observable<any> {
+        let user: UserModel = JSON.parse(this.storageService.getAuth() || '{}'),
+            token = user && user.token;
+
+        var observer = new Observable((observer)=>{
+            const {error} = observer;
+
+            if (!token) {
+                error({
+                    status: false,
+                    message: 'Token old'
+                })
+            }
+        });
+
+        if (token) {
+            this.headers.append('Content-Type', 'application/json');
+            this.headers.append('Authorization', 'Bearer ' + token);
+
+            observer = this.httpClient.post('/is-auth/', {}, {headers: this.headers});
+        }
+
+        return observer;
+    }
 
     signIn(body: {}): Observable<any> {
         return this.httpClient.post('/sign-in/', body);
